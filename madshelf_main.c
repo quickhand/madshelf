e7e8175607d0eb705c0ef40591d3a604a4cc3d28
Mikhail Gusarov <dottedmag@dottedmag.net>
2008-07-06 02:55:05 +0700

    Copyright notices updated

--- madshelf_main.c
+++ madshelf_main.c
@@ -1,5 +1,5 @@
 /* ------------------------------------------------------------------------
- Copyright (C) 2008 by Marc Lajoie
+ Copyright (C) 2008 by Marc Lajoie <quickhand@openinkpot.org>
 
  This program is free software; you can redistribute it and/or modify it under
  the terms of the GNU General Public License as published by the Free Software
